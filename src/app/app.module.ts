345e4e2fae38059b0ac2648736ff8f6a2ed26993
CodeHereOfficial <snehal.rajapurkar@cognizant.com>
2022-08-06 22:12:36 +0530

    5 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { ProductListComponent } from './products/products.component';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import {MatCardModule} from '@angular/material/card';
 // import {ProductListComponent} from './products.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, HttpClientModule, AppRoutingModule ],
+  imports:      [ BrowserModule, FormsModule, HttpClientModule, AppRoutingModule, MatCardModule ],
   declarations: [ AppComponent, HelloComponent, ProductListComponent ],
   bootstrap:    [ AppComponent ]
 })
