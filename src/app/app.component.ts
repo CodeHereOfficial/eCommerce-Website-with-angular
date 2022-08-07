3c561b7a63bf07c01e530acab1042ff8e3dd552e
CodeHereOfficial <snehal.rajapurkar@cognizant.com>
2022-08-07 18:19:18 +0530

    4 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, OnDestroy, OnInit, VERSION } from '@angular/core';
+import {MediaObserver, MediaChange} from '@angular/flex-layout';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent implements OnInit, OnDestroy {
+  mediaSub: Subscription;
+  deviceXs : boolean;
+
+  constructor( public meadiaObserver : MediaObserver){}
+
+  ngOnInit (){
+this.mediaSub = this.meadiaObserver.media$.subscribe(
+  (result: MediaChange) => {
+    console.log(result.mqAlias);
+    this.deviceXs = result.mqAlias === 'xs' ? true : false;
+  }
+);
+  }
+  ngOnDestroy(){
+    this.mediaSub.unsubscribe();
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,20 @@ import { HelloComponent } from './hello.component';
 import { ProductListComponent } from './products/products.component';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
-import {MatCardModule} from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatProgressBarModule } from "@angular/material/progress-bar";
-import {MatGridListModule} from '@angular/material/grid-list';
+import { MaterialModule } from './core/material.module';
+
 
 // import {ProductListComponent} from './products.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, HttpClientModule, AppRoutingModule, MatCardModule, MatButtonModule, MatDividerModule, MatProgressBarModule, MatGridListModule ],
-  declarations: [ AppComponent, HelloComponent, ProductListComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    MaterialModule
+  ],
+  declarations: [AppComponent, HelloComponent, ProductListComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
