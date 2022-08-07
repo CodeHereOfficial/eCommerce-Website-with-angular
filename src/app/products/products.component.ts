975e5b9bdf2c7100eba2a4cd528590f2b83b6e92
CodeHereOfficial <snehal.rajapurkar@cognizant.com>
2022-08-07 14:02:32 +0530

    3 files modified, 1 added

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,23 +1,5 @@
-
 import { Component, OnInit } from '@angular/core';
 import { ProductDataService } from '../services/product-data.service';
-
-
-export class Product {
-  constructor(
-    public id: number,
-    public title: string,
-    public description: string,
-    public price: number,
-    public discountPercentage: number,
-    public rating: number,
-    public stock: number,
-    public brand: string,
-    public category: string,
-    public thumbnail: any,
-    public images: any
-  ) {}
-}
 
 @Component({
   selector: 'app-product-list',
@@ -25,17 +7,16 @@ export class Product {
   styleUrls: ['./products.component.css'],
 })
 export class ProductListComponent implements OnInit {
-  products: Product[] = [];
-  constructor(private product : ProductDataService) {
-    this.product.getProducts().subscribe((data:any[])=>{
-this.products = data;
-console.log(data)
-    });
+  products: any = [];
+  constructor(private product: ProductDataService) {
+    //     this.product.getProducts().subscribe((data:any[])=>{
+    // this.products = data;
+    // console.log(data)
   }
 
   ngOnInit(): void {
-    
+    this.product.getProducts().subscribe((data: any[]) => {
+      this.products = data;
+    });
   }
-
-  
 }
--- src/app/services/product-data.service.ts
+++ src/app/services/product-data.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IProduct } from '../product';
 
 @Injectable({
   providedIn: 'root',
@@ -8,7 +10,7 @@ export class ProductDataService {
   url: any = 'https://dummyjson.com/products';
   constructor(private http: HttpClient) {}
 
-  getProducts() {
-    return this.http.get(this.url);
+  getProducts():Observable<IProduct[]> {
+    return this.http.get<IProduct[]>(this.url);
   }
 }
