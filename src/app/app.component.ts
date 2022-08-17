29fe6d0ce1fe6b8b6f8470e85de6f1749008d14c
CodeHereOfficial <snehal.rajapurkar@cognizant.com>
2022-08-17 15:01:20 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  images = ['https://t3.ftcdn.net/jpg/03/20/68/66/360_F_320686681_Ur6vdYQgDC9WiijiVfxlRyQffxOgfeFz.jpg','https://img.freepik.com/free-photo/black-friday-elements-assortment_23-2149074076.jpg?w=2000', 'https://images.ctfassets.net/rxqefefl3t5b/6I2vL9f0IVsDQ8qFgdrxH7/7660c4bab3116a4a04025d5c4802efa5/Virgin-Red-online-shopping-offers.jpg', 'https://thumbs.dreamstime.com/b/little-girl-pointing-something-family-shopping-parents-daughter-copy-space-holding-shopper-bags-yellow-studio-156232563.jpg']
+  images = ['https://cdn11.bigcommerce.com/s-7z9nkigk30/product_images/uploaded_images/getting-mini.png?t=1601916808&_ga=2.84628057.1959320340.1601915546-1542306807.1592319001','https://img.freepik.com/free-photo/woman-holding-various-shopping-bags-copy-space_23-2148674122.jpg?w=2000', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-Uotv8Cy8ZdvUlgrJ8ilycLdkvPWUqVueFA&usqp=CAU', 'https://thumbs.dreamstime.com/b/little-girl-pointing-something-family-shopping-parents-daughter-copy-space-holding-shopper-bags-yellow-studio-156232563.jpg']
   
 }
