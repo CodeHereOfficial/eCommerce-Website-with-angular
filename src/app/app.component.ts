7151f7f658f8b0a1236a009217938e893f9c77bc
CodeHereOfficial <snehal.rajapurkar@cognizant.com>
2022-08-17 17:37:32 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  images = ['https://cdn11.bigcommerce.com/s-7z9nkigk30/product_images/uploaded_images/getting-mini.png?t=1601916808&_ga=2.84628057.1959320340.1601915546-1542306807.1592319001','https://img.freepik.com/free-photo/woman-holding-various-shopping-bags-copy-space_23-2148674122.jpg?w=2000', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-Uotv8Cy8ZdvUlgrJ8ilycLdkvPWUqVueFA&usqp=CAU', 'https://thumbs.dreamstime.com/b/little-girl-pointing-something-family-shopping-parents-daughter-copy-space-holding-shopper-bags-yellow-studio-156232563.jpg']
-  
+  images = [
+    'https://thumbs.dreamstime.com/b/little-girl-pointing-something-family-shopping-parents-daughter-copy-space-holding-shopper-bags-yellow-studio-156232563.jpg',
+    'https://img.freepik.com/free-photo/woman-holding-various-shopping-bags-copy-space_23-2148674122.jpg?w=2000',
+    'https://cdn11.bigcommerce.com/s-7z9nkigk30/product_images/uploaded_images/getting-mini.png?t=1601916808&_ga=2.84628057.1959320340.1601915546-1542306807.1592319001',
+  ];
 }
