8f8778f8fd87dedf120341dcac3423ca182e87c6
CodeHereOfficial <snehal.rajapurkar@cognizant.com>
2022-08-19 14:44:22 +0530

    1 file modified, 2 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,6 @@ import { Component } from '@angular/core';
 export class AppComponent {
   images = [
     'https://thumbs.dreamstime.com/b/little-girl-pointing-something-family-shopping-parents-daughter-copy-space-holding-shopper-bags-yellow-studio-156232563.jpg',
-    'https://img.freepik.com/free-photo/woman-holding-various-shopping-bags-copy-space_23-2148674122.jpg?w=2000',
-    'https://cdn11.bigcommerce.com/s-7z9nkigk30/product_images/uploaded_images/getting-mini.png?t=1601916808&_ga=2.84628057.1959320340.1601915546-1542306807.1592319001','https://media.istockphoto.com/photos/pretty-young-woman-stylishly-dressed-in-a-straw-hat-with-color-bags-picture-id1033729610?k=20&m=1033729610&s=612x612&w=0&h=56u-S8LPU2gL5CO3kSF9wq_PV4kSY5oVl7Hhm-0fshg=',
+    'https://cdn11.bigcommerce.com/s-7z9nkigk30/product_images/uploaded_images/getting-mini.png?t=1601916808&_ga=2.84628057.1959320340.1601915546-1542306807.1592319001','https://media.istockphoto.com/photos/pretty-young-woman-stylishly-dressed-in-a-straw-hat-with-color-bags-picture-id1033729610?k=20&m=1033729610&s=612x612&w=0&h=56u-S8LPU2gL5CO3kSF9wq_PV4kSY5oVl7Hhm-0fshg=','https://img.freepik.com/free-photo/woman-holding-various-shopping-bags-copy-space_23-2148674122.jpg?w=2000'
   ];
 }
